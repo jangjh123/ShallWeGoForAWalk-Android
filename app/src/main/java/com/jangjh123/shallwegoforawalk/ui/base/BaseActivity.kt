eb81fc17b2db56fa2156816249ec4f9075db1bf3
gn000123 <gn000123@naver.com>
2022-05-01 21:42:13 +0900

    [feat] RegisterActivity 구현
    View 는 모두 구현하였으며, 데이터 패싱 구현 예정임.

--- app/src/main/java/com/jangjh123/shallwegoforawalk/ui/base/BaseActivity.kt
+++ app/src/main/java/com/jangjh123/shallwegoforawalk/ui/base/BaseActivity.kt
@@ -1,9 +1,16 @@
 package com.jangjh123.shallwegoforawalk.ui.base
 
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+
 
 abstract class BaseActivity<VB : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
     lateinit var binding: VB
@@ -16,6 +23,7 @@ abstract class BaseActivity<VB : ViewDataBinding>(private val layoutId: Int) : A
     protected open fun init() {
         initViewDataBinding()
         startProcess()
+        setObserver()
     }
 
     protected open fun initViewDataBinding() {
@@ -40,12 +48,29 @@ abstract class BaseActivity<VB : ViewDataBinding>(private val layoutId: Int) : A
 //        }
 //    }
 
-    protected open fun getMessages() {
-
-    }
-
     override fun onDestroy() {
         super.onDestroy()
 //        dismissProgress()
+    }
+
+    protected fun playAnimation(view: View, anim: Int) {
+        view.startAnimation(AnimationUtils.loadAnimation(this, anim))
+    }
+
+    protected fun playColorAnimation(view: View, startColor: Int, endColor: Int) {
+        ValueAnimator.ofObject(ArgbEvaluator(), startColor, endColor).apply {
+            duration = 250
+            addUpdateListener { animator -> view.setBackgroundColor(animator.animatedValue as Int) }
+        }.start()
+    }
+
+    protected fun color(color: Int) =
+        ContextCompat.getColor(this, color)
+
+    protected fun drawable(drawable: Int) =
+        AppCompatResources.getDrawable(this, drawable)
+
+    protected open fun setObserver() {
+
     }
 }
