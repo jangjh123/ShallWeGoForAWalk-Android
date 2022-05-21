69cda148924d5049ca610e6e992c8d129d8e586e
gn000123 <gn000123@naver.com>
2022-05-22 02:25:54 +0900

    [feat] RecyclerView SnapHelper 설정 기능 추가
    BindingAdapter.kt 에 구현하여, XML 에서 바로 사용할 수 있음.

--- app/src/main/java/com/jangjh123/shallwegoforawalk/binding/BindingAdapter.kt
+++ app/src/main/java/com/jangjh123/shallwegoforawalk/binding/BindingAdapter.kt
@@ -2,7 +2,9 @@ package com.jangjh123.shallwegoforawalk.binding
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 
 
 object BindingAdapter {
@@ -10,5 +12,13 @@ object BindingAdapter {
     @BindingAdapter("setLayoutManager")
     fun setLayoutManager(view: RecyclerView, orientation: Int) {
         view.layoutManager = LinearLayoutManager(view.context, orientation, false)
+    }
+
+    @JvmStatic
+    @BindingAdapter("setSnapHelper")
+    fun setSnapHelper(view: RecyclerView, boolean: Boolean) {
+        if (boolean) {
+            PagerSnapHelper().attachToRecyclerView(view)
+        }
     }
 }
