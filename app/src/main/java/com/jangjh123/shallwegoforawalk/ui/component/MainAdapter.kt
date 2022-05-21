a4fdee4a1e130d5b2273309e3d2eb7990bf0e69c
gn000123 <gn000123@naver.com>
2022-05-21 23:53:47 +0900

    [design] View QA

---
+++ app/src/main/java/com/jangjh123/shallwegoforawalk/ui/component/MainAdapter.kt
@@ -0,0 +1,38 @@
+package com.jangjh123.shallwegoforawalk.ui.component
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.jangjh123.shallwegoforawalk.data.DogWeather
+import com.jangjh123.shallwegoforawalk.databinding.ItemMainBinding
+import com.jangjh123.shallwegoforawalk.util.GenericDiffUtil
+
+class MainAdapter :
+    ListAdapter<DogWeather, RecyclerView.ViewHolder>(GenericDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return ViewHolder(
+            ItemMainBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewHolder) {
+            val dogWeather = getItem(position)
+            holder.bind(dogWeather)
+        }
+    }
+
+    inner class ViewHolder(private val binding: ItemMainBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(dogWeather: DogWeather) {
+            with(binding) {
+                textviewDogName.text = dogWeather.name
+            }
+        }
+    }
+}
